be72088e755d5f2c5f8caaf562a4e7bded82c1bf
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-19 20:57:29 -0300

    Opções novas

--- plano.py
+++ plano.py
@@ -2,22 +2,34 @@
 Ferramenta de extração do plano de bit.
 """
 from argparse import ArgumentTypeError
+from enum import IntEnum, unique
+import numpy as np
 from lib.args import Argumentos
 from lib.inout import imgshow, imgwrite
 
 
-# canais válidos
-CORES = ['B', 'G', 'R']
+@unique
+class Cor(IntEnum):
+    """
+    Canais de cores válidos na imagem.
+    """
+    B = 0
+    G = 1
+    R = 2
 
-def canal_de_cor(cor: str) -> int:
-    """
-    Leitura de canal de cor.
-    """
-    try:
-        return CORES.index(cor)
-    except ValueError as err:
-        msg = 'canal de cor inválido'
-        raise ArgumentTypeError(msg) from err
+    @classmethod
+    def parse(cls, cor: str) -> 'Cor':
+        """
+        Leitura de canal de cor.
+        """
+        try:
+            return cls[cor]
+        except ValueError as err:
+            msg = 'canal de cor inválido'
+            raise ArgumentTypeError(msg) from err
+
+    def __str__(self) -> str:
+        return str(self.name)
 
 
 # parser de argumentos
@@ -25,10 +37,9 @@ parser = Argumentos('Ferramenta de extração do plano de bit.')
 parser.add_argument('-i', '--inverte', action='store_true',
                     help='inverte resultado')
 # opções de extração de canal de cores
-parser.add_argument('-c', '--cor', choices=CORES, type=canal_de_cor,
+parser.add_argument('-c', '--cor', choices=Cor, type=Cor.parse,
                     help='extração de um canal de cor (padrão: todos)')
-parser.add_argument('-d', '--fundo-branco', dest='fundo',
-                    action='store_const', default=0, const=255,
+parser.add_argument('-d', '--fundo-branco', action='store_true',
                     help='extração de canal em fundo branco (padrão: preto)')
 parser.add_argument('-g', '--grayscale', action='store_true',
                     help='extração de canal de cor em escala de cinza')
@@ -54,10 +65,17 @@ if __name__ == '__main__':
         # para escala de cinza
         if args.grayscale:
             img = img[..., args.cor]
-        # para imagem colorida
+        # para imagem colorida com fundo preto
+        elif not args.fundo_branco:
+            idx = [c for c in Cor if c != args.cor]
+            img[..., idx] = 0
+        # e com fundo branco
         else:
-            outros = [c for c in CORES if c != args.cor]
-            img[..., outros] = args.fundo
+            for c in Cor:
+                if c == args.cor:
+                    continue
+                img[..., c] = ~img[..., args.cor]
+            img[..., args.cor] = 255
 
     # exibição do resultado
     if args.show or args.output is None:
