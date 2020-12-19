e0efbf07f81e164fd8e1ab60e321bb6fe03c0b84
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-19 19:50:18 -0300

    Opções extras

--- plano.py
+++ plano.py
@@ -1,13 +1,38 @@
 """
 Ferramenta de extração do plano de bit.
 """
+from argparse import ArgumentTypeError
 from lib.args import Argumentos
 from lib.inout import imgshow, imgwrite
 
 
-DESCRICAO = 'Ferramenta de extração do plano de bit.'
+# canais válidos
+CORES = ['B', 'G', 'R']
+
+def canal_de_cor(cor: str) -> int:
+    """
+    Leitura de canal de cor.
+    """
+    try:
+        return CORES.index(cor)
+    except ValueError as err:
+        msg = 'canal de cor inválido'
+        raise ArgumentTypeError(msg) from err
+
+
 # parser de argumentos
-parser = Argumentos(DESCRICAO)
+parser = Argumentos('Ferramenta de extração do plano de bit.')
+parser.add_argument('-i', '--inverte', action='store_true',
+                    help='inverte resultado')
+# opções de extração de canal de cores
+parser.add_argument('-c', '--cor', choices=CORES, type=canal_de_cor,
+                    help='extração de um canal de cor (padrão: todos)')
+parser.add_argument('-d', '--fundo-branco', dest='fundo',
+                    action='store_const', default=0, const=255,
+                    help='extração de canal em fundo branco (padrão: preto)')
+parser.add_argument('-g', '--grayscale', action='store_true',
+                    help='extração de canal de cor em escala de cinza')
+
 
 if __name__ == '__main__':
     args = parser.parse_intermixed_args()
@@ -19,6 +44,20 @@ if __name__ == '__main__':
     img = img >> bit
     img = img & 1
     img = 255 * img
+
+    # inversão do resultado
+    if args.inverte:
+        img = ~img
+
+    # extração de um canal de cor
+    if args.cor is not None:
+        # para escala de cinza
+        if args.grayscale:
+            img = img[..., args.cor]
+        # para imagem colorida
+        else:
+            outros = [c for c in CORES if c != args.cor]
+            img[..., outros] = args.fundo
 
     # exibição do resultado
     if args.show or args.output is None:
