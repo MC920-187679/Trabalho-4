70e5081858f9fce059e8f530f7fb20c44e414d94
Tiago de Paula <tiagodepalves@gmail.com>
2020-12-30 18:08:34 -0300

    Planos diferenciados

--- run.sh
+++ run.sh
@@ -41,22 +41,26 @@
 # python3 plano.py -o resultados/watch_mona/pl0chg.png -c g resultados/watch_mona/imagem.png
 # python3 plano.py -o resultados/watch_mona/pl0chr.png -c r resultados/watch_mona/imagem.png
 
-# peppers com permutação
-mkdir -p resultados/pep_perm
-python3 codificar.py -o resultados/pep_perm/imagem.png -p imagens/peppers.png textos/enunciado.md
-python3 plano.py -o resultados/pep_perm/plano0.png  resultados/pep_perm/imagem.png
-python3 plano.py -o resultados/pep_perm/pl0chb.png -c b resultados/pep_perm/imagem.png
-python3 plano.py -o resultados/pep_perm/pl0chg.png -c g resultados/pep_perm/imagem.png
-python3 plano.py -o resultados/pep_perm/pl0chr.png -c r resultados/pep_perm/imagem.png
+# # peppers com permutação
+# mkdir -p resultados/pep_perm
+# python3 codificar.py -o resultados/pep_perm/imagem.png -p imagens/peppers.png textos/enunciado.md
+# python3 plano.py -o resultados/pep_perm/plano0.png  resultados/pep_perm/imagem.png
+# python3 plano.py -o resultados/pep_perm/pl0chb.png -c b resultados/pep_perm/imagem.png
+# python3 plano.py -o resultados/pep_perm/pl0chg.png -c g resultados/pep_perm/imagem.png
+# python3 plano.py -o resultados/pep_perm/pl0chr.png -c r resultados/pep_perm/imagem.png
 
+# # watch com permutação
+# mkdir -p resultados/watch_perm
+# python3 codificar.py -o resultados/watch_perm/imagem.png -p imagens/watch.png imagens/monalisa.png
+# python3 plano.py -o resultados/watch_perm/plano0.png  resultados/watch_perm/imagem.png
+# python3 plano.py -o resultados/watch_perm/pl0chb.png -c b resultados/watch_perm/imagem.png
+# python3 plano.py -o resultados/watch_perm/pl0chg.png -c g resultados/watch_perm/imagem.png
+# python3 plano.py -o resultados/watch_perm/pl0chr.png -c r resultados/watch_perm/imagem.png
 
-# watch com permutação
-mkdir -p resultados/watch_perm
-python3 codificar.py -o resultados/watch_perm/imagem.png -p imagens/watch.png imagens/monalisa.png
-python3 plano.py -o resultados/watch_perm/plano0.png  resultados/watch_perm/imagem.png
-python3 plano.py -o resultados/watch_perm/pl0chb.png -c b resultados/watch_perm/imagem.png
-python3 plano.py -o resultados/watch_perm/pl0chg.png -c g resultados/watch_perm/imagem.png
-python3 plano.py -o resultados/watch_perm/pl0chr.png -c r resultados/watch_perm/imagem.png
+# planos diferenciados
+mkdir -p resultados/bits
+python3 codificar.py -o resultados/bits/mona.png -b 3 imagens/monalisa.png textos/enunciado.md
+head -n 5000 textos/words.txt | python3 codificar.py -o resultados/bits/pep.png -b 3 imagens/peppers.png
 
 # verficação
 python3 decodificar.py resultados/mona_assin/imagem.png | diff -q - <(echo Realizzato da Leonardo da Vinci™)
@@ -66,3 +70,5 @@ python3 decodificar.py resultados/bab_zip/imagem.png | diff -qb - resultados/bab
 python3 decodificar.py resultados/watch_mona/imagem.png | diff -qb - imagens/monalisa.png
 python3 decodificar.py resultados/pep_perm/imagem.png | diff -q - textos/enunciado.md
 python3 decodificar.py resultados/watch_perm/imagem.png | diff -qb - imagens/monalisa.png
+python3 decodificar.py -b 3 resultados/bits/mona.png | diff -q - textos/enunciado.md
+python3 decodificar.py -b 3 resultados/bits/pep.png | diff -q - <(head -n 5000 textos/words.txt)
